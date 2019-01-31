b1c039275d30e17d6a9332f75ac2e178948c3a6b
Lakshmi Reddy <saisrilakshmi@sais-mbp.nwmissouri.edu>
2019-01-31 11:38:19 -0600

    Hello code

--- Hello xcode/ViewController.swift
+++ Hello xcode/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLBL: UILabel!
+    
+    @IBOutlet weak var nameTF: UITextField!
+    
+    @IBAction func clickMe(_ sender: Any) {
+       let phrases  = ["Hello", "Howdy","Namaste","Ola", "Bonjour"]
+        let index = Int.random(in:0..<phrases.count)
+        helloLBL.text = "\( phrases[index]), \(nameTF.text!)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
